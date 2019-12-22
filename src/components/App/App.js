a437a85df49baf108c970519fdebbe348ab54fd0
TheMindset <mulezig2014@outlook.fr>
2019-12-22 18:52:50 +0100

    Stylize Menu & make it dynamic

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,20 +3,39 @@ import local from '../../data/local';
 import './App.css';
 import NewsContainer from '../NewsContainer/NewsContainer'
 import Menu from '../Menu/Menu'
+import entertainment from '../../data/entertainment'
+import health from '../../data/science'
+import science from '../../data/science'
+import technology from '../../data/technology'
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      local
+      local,
+      health,
+      entertainment,
+      science,
+      technology
     }
+    this.state.current = local
+
+  }
+
+
+  handleButton = event => {
+    const { name } = event.target
+    console.log(name)
+    this.setState({ 
+      current: this.state[name]
+    })
   }
 
   render () {
     return (
       <div className="app">
-        <Menu />
-        <NewsContainer news={this.state.local} />
+        <Menu handleButton={this.handleButton}/>
+        <NewsContainer news={this.state.current} />
       </div>
     );
   }
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './Menu.css'
 
-const Menu = () => {
+const Menu = ({handleButton}) => {
   return (
     <nav>
       <h2>What's <span>New?</span></h2>
-      <button name="health"> Health</button>
-      <button name="entertainment">Entertainment</button>
-      <button name="science">Science</button>
-      <button name="technology">Technology</button>
-      <button name="local">Local</button>
+      <button name="local" type="button" onClick={(e) => handleButton(e)}>Local</button>
+      <button name="health" type="button" onClick={(e) => handleButton(e)}> Health</button>
+      <button name="entertainment" type="button" onClick={(e) => handleButton(e)}>Entertainment</button>
+      <button name="science" type="button" onClick={(e) => handleButton(e)}>Science</button>
+      <button name="technology"type="button" onClick={(e) => handleButton(e)}>Technology</button>
     </nav>
   )
 }
